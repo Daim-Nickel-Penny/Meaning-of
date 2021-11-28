466dcb050735fefbf6f6cfbb0d600a3cb9e689cf
1804449@kiit.ac.in <1804449@kiit.ac.in>
2021-11-28 13:22:39 +0530

    PopUp Modal Desiging Done

---
+++ popup.js
@@ -0,0 +1,31 @@
+"use strict";
+
+let control = document.getElementById("dictionEnabled");
+chrome.storage.sync.get("isEnabled", function (data) {
+  control.checked = data.isEnabled;
+});
+control.onchange = function () {
+  chrome.storage.sync.set(
+    {
+      isEnabled: control.checked,
+    },
+    () => {
+      console.log(`Meaning Of ${control.checked ? "enabled" : "disabled"}`);
+    }
+  );
+};
+
+let anchor = document.getElementById("meaningOfAnchor");
+chrome.storage.sync.get("anchor", function (data) {
+  anchor.value = data.anchor;
+});
+anchor.onchange = function () {
+  chrome.storage.sync.set(
+    {
+      anchor: anchor.value,
+    },
+    () => {
+      console.log(`Meaning Of anchored at ${anchor.value}`);
+    }
+  );
+};
