f1adf764e7fd018dd0526f24a38aa874b2ad865d
1804449@kiit.ac.in <1804449@kiit.ac.in>
2021-11-28 13:14:36 +0530

    Init done

---
+++ background.js
@@ -0,0 +1,13 @@
+"use strict";
+
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.set(
+    {
+      isEnabled: true,
+      anchor: "br",
+    },
+    function () {
+      console.log("Diction Initialized.");
+    }
+  );
+});
